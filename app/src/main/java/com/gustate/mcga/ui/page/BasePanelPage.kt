b9cb1a4a55f6672e94eea3b7cd30d5baf7ef26f7
JiaGuZhuangZhi <2280718448@qq.com>
2025-11-18 01:18:51 +0800

    #add 新增 BasePanelPage 可组函数

---
+++ app/src/main/java/com/gustate/mcga/ui/page/BasePanelPage.kt
@@ -0,0 +1,69 @@
+package com.gustate.mcga.ui.page
+
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import com.gustate.mcga.ui.widget.AppTopBar
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalSharedTransitionApi::class)
+@Composable
+fun BasePanelPage(
+    title: String,
+    onBackClick: () -> Unit,
+    onRestartClick: () -> Unit,
+    sharedKey: String,
+    sharedTransitionScope: SharedTransitionScope,
+    animatedVisibilityScope: AnimatedVisibilityScope,
+    content: @Composable (
+        paddingValues: PaddingValues,
+        scrollBehavior: TopAppBarScrollBehavior
+    ) -> Unit
+) {
+    val scrollBehavior = TopAppBarDefaults
+        .exitUntilCollapsedScrollBehavior(state = rememberTopAppBarState())
+    with(receiver = sharedTransitionScope) {
+        Scaffold(
+            topBar = {
+                AppTopBar(
+                    title = {
+                        Text(
+                            text = title,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    },
+                    scrollBehavior = scrollBehavior,
+                    isBackAvailable = true,
+                    onBackClick = onBackClick,
+                    isRestartAvailable = true,
+                    onRestartClick = onRestartClick
+                )
+            },
+            modifier = Modifier
+                .fillMaxSize()
+                .sharedBounds(
+                    sharedContentState = rememberSharedContentState(key = sharedKey),
+                    animatedVisibilityScope = animatedVisibilityScope,
+                    enter = fadeIn(),
+                    exit = fadeOut(),
+                    resizeMode = SharedTransitionScope.ResizeMode.ScaleToBounds()
+                )
+        ) { paddingValues ->
+            content(paddingValues, scrollBehavior)
+        }
+    }
+}
