4ee0c7959f5bec994ee49792194acf7def63af03
JiaGuZhuangZhi <2280718448@qq.com>
2025-11-18 01:25:29 +0800

    #feature Log 工具

--- app/src/main/java/com/gustate/mcga/utils/LogUtils.kt
+++ app/src/main/java/com/gustate/mcga/utils/LogUtils.kt
@@ -4,7 +4,7 @@ import de.robv.android.xposed.XposedBridge
 
 object LogUtils {
     fun log(message: String, tag: String = "null", throwable: Throwable? = null) {
-        val fullMessage = "[$tag] $message"
+        val fullMessage = "[MCGA-$tag] $message"
         if (throwable != null) XposedBridge.log(throwable)
         else XposedBridge.log(fullMessage)
     }
