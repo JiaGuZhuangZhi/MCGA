4045284acea4c8b22aad3d7e38d508203cc0a106
JiaGuZhuangZhi <2280718448@qq.com>
2025-11-18 01:22:08 +0800

    #fix 读 sp 闪退

--- app/src/main/java/com/gustate/mcga/data/XposedRepo.kt
+++ app/src/main/java/com/gustate/mcga/data/XposedRepo.kt
@@ -7,14 +7,34 @@ import androidx.core.content.edit
 class XposedRepo(private val context: Context) {
 
     @SuppressLint("WorldReadableFiles")
-    private val prefs = context
-        .getSharedPreferences("xposed_prefs", Context.MODE_WORLD_READABLE)
+    private val prefs =
+        try {
+            context.getSharedPreferences("xposed_prefs", Context.MODE_WORLD_READABLE)
+        } catch (_: SecurityException) {
+            null
+        }
+
+    fun isModuleActive(): Boolean = prefs != null
 
     fun getBoolean(key: String, def: Boolean = false): Boolean =
-        prefs.getBoolean(key, def)
+        prefs?.getBoolean(key, def) ?: def
 
     fun setBoolean(key: String, value: Boolean) {
-        prefs.edit { putBoolean(key, value) }
+        prefs?.edit { putBoolean(key, value) }
+    }
+
+    fun getFloat(key: String, def: Float = 0f): Float =
+        prefs?.getFloat(key, def) ?: def
+
+    fun setFloat(key: String, value: Float) {
+        prefs?.edit { putFloat(key, value) }
+    }
+
+    fun getInt(key: String, def: Int = 0): Int =
+        prefs?.getInt(key, def) ?: def
+
+    fun setInt(key: String, value: Int) {
+        prefs?.edit { putInt(key, value) }
     }
 
 }
