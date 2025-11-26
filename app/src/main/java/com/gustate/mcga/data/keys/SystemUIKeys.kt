e5d7b740670bc05ef1e012c66e855fd265942e87
JiaGuZhuangZhi <2280718448@qq.com>
2025-11-26 19:15:11 +0800

    #add 新增双格磁铁圆角设置

--- app/src/main/java/com/gustate/mcga/data/keys/SystemUIKeys.kt
+++ app/src/main/java/com/gustate/mcga/data/keys/SystemUIKeys.kt
@@ -3,6 +3,10 @@ package com.gustate.mcga.data.keys
 object SystemUIKeys {
     const val ENABLE_CUSTOM_QS_DETAIL = "enable_custom_qs_detail"
     const val QS_DETAIL_BKG_COVER_COLOR = "qs_detail_cover_color"
+    const val QS_DETAIL_FRG_COVER_COLOR = "qs_detail_cover_frg_color"
     const val QS_DETAIL_BKG_BLUR_RADIUS = "qs_detail_blur_radius"
     const val QS_DETAIL_BKG_CORNER_RADIUS = "qs_detail_corner_radius"
+    const val ENABLE_CUSTOM_QS_RESIZEABLE_TILE = "enable_custom_qs_resizeable_tile"
+    const val QS_RESIZEABLE_TILE_CORNER_RADIUS = "qs_resizeable_tile_corner_radius"
+    const val ENABLE_AOD_PANORAMIC_ALL_DAY = "enable_aod_panoramic_all_day"
 }
