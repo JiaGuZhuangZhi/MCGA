bf160ae3e7eee1bf888ac3641ab555c1389d69f0
JiaGuZhuangZhi <2280718448@qq.com>
2025-11-26 19:14:33 +0800

    #add 新增前景覆盖颜色

--- app/src/main/java/com/gustate/mcga/xposed/systemui/SystemuiHook.kt
+++ app/src/main/java/com/gustate/mcga/xposed/systemui/SystemuiHook.kt
@@ -2,6 +2,8 @@ package com.gustate.mcga.xposed.systemui
 
 import android.annotation.SuppressLint
 import com.gustate.mcga.data.keys.SystemUIKeys
+import com.gustate.mcga.xposed.systemui.feature.Aod
+import com.gustate.mcga.xposed.systemui.feature.QSTile
 import com.gustate.mcga.xposed.systemui.feature.QsDetail
 import de.robv.android.xposed.XSharedPreferences
 import de.robv.android.xposed.callbacks.XC_LoadPackage
@@ -16,12 +18,17 @@ object SystemuiHook {
             "xposed_prefs"
         )
         prefs.makeWorldReadable()
+        // QS DETAIL
         val enableCustomQsDetail = prefs.getBoolean(
             SystemUIKeys.ENABLE_CUSTOM_QS_DETAIL,
             false
         )
         val qsDetailCoverColor = prefs.getInt(
             SystemUIKeys.QS_DETAIL_BKG_COVER_COLOR,
+            0X80000000.toInt()
+        )
+        val qsDetailFrgColor = prefs.getInt(
+            SystemUIKeys.QS_DETAIL_FRG_COVER_COLOR,
             0X80000000.toInt()
         )
         val qsDetailBlurRadius = prefs.getInt(
@@ -37,8 +44,32 @@ object SystemuiHook {
                 lpparam = lpparam,
                 blurRadius = qsDetailBlurRadius,
                 cornerRadius = qsDetailCornerRadius,
+                foregroundColor = qsDetailFrgColor,
                 backgroundColor = qsDetailCoverColor
             )
+        }
+        // Resizeable Tile
+        val enableCustomQsResizeableTile = prefs.getBoolean(
+            SystemUIKeys.ENABLE_CUSTOM_QS_RESIZEABLE_TILE,
+            false
+        )
+        val qsResizeableTileCornerRadius = prefs.getFloat(
+            SystemUIKeys.QS_RESIZEABLE_TILE_CORNER_RADIUS,
+            28.0f
+        )
+        if (enableCustomQsResizeableTile) {
+            QSTile.hookQSResizeableTile(
+                lpparam = lpparam,
+                cornerRadius = qsResizeableTileCornerRadius
+            )
+        }
+        // AOD
+        val enableAodPanoramicAllDay = prefs.getBoolean(
+            SystemUIKeys.ENABLE_AOD_PANORAMIC_ALL_DAY,
+            false
+        )
+        if (enableAodPanoramicAllDay) {
+            Aod.hookPanoramicAodAllDay(lpparam)
         }
     }
 }
