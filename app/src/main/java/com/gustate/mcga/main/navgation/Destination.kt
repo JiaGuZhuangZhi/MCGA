ca31c7a7dc2095524730b41f00bcb193e2cf19e0
JiaGuZhuangZhi <2280718448@qq.com>
2025-11-26 19:17:45 +0800

    #fix 主页导航错误

--- app/src/main/java/com/gustate/mcga/main/navgation/Destination.kt
+++ app/src/main/java/com/gustate/mcga/main/navgation/Destination.kt
@@ -5,7 +5,6 @@ import androidx.annotation.StringRes
 import com.gustate.mcga.R
 
 enum class Destination(
-    val route: String,
     @param:StringRes @field:StringRes
     val label: Int,
     @param:DrawableRes @field:DrawableRes
@@ -16,19 +15,19 @@ enum class Destination(
     val contentDescription: Int
 ) {
     SETTING(
-        route = "setting", label = R.string.setting,
+        label = R.string.setting,
         icon = R.drawable.settings_outline,
         focusIcon = R.drawable.settings_filled,
         contentDescription = R.string.setting
     ),
     HOME(
-        route = "home", label = R.string.home,
+        label = R.string.home,
         icon = R.drawable.home_outline,
         focusIcon = R.drawable.home_filled,
         contentDescription = R.string.home
     ),
     ABOUT(
-        route = "about", label = R.string.about,
+        label = R.string.about,
         icon = R.drawable.info_outline,
         focusIcon = R.drawable.info_filled,
         contentDescription = R.string.home
