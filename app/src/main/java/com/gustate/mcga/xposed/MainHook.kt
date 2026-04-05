85bb4cbefe1ee5c28e435278d10fa09e9147da27
JiaGuZhuangZhi <2280718448@qq.com>
2026-04-05 12:45:51 +0800

    chore(MainHook): 构造函数迁移至 MainHook

--- app/src/main/java/com/gustate/mcga/xposed/MainHook.kt
+++ app/src/main/java/com/gustate/mcga/xposed/MainHook.kt
@@ -1,19 +1,84 @@
 package com.gustate.mcga.xposed
 
+import android.content.SharedPreferences
+import com.gustate.mcga.xposed.aod.AodHook
+import com.gustate.mcga.xposed.home.HomeHook
+import com.gustate.mcga.xposed.search.SearchHook
+import com.gustate.mcga.xposed.systemui.SystemuiHook
+import com.gustate.mcga.xposed.wallet.WalletHook
+import io.github.libxposed.api.XposedModule
+import io.github.libxposed.api.XposedModuleInterface
 
-/*class MainHook : XposedModule() {
+class MainHook : XposedModule() {
 
+    /**
+     * 成功实例化软件包加载器
+     * @param param 正在装载的软件包信息
+     */
     override fun onPackageReady(param: XposedModuleInterface.PackageReadyParam) {
         super.onPackageReady(param)
-        applyAllFeature(param = param)
+
+        // 获取 Lsposed 远程配置
+        val prefs = getRemotePreferences("mcga_prefs")
+
+        // 应用全部 Hook 设置
+        applyAllFeature(
+            param = param,
+            prefs = prefs
+        )
     }
 
-    private fun applyAllFeature(param: XposedModuleInterface.PackageReadyParam) {
-        HomeHook.applySearchFeature(param)
-        SearchHook.applySearchFeature(param)
-        SystemuiHook.applySystemUiFeature(param)
-        AodHook.applyAodFeature(param)
-        WalletHook.applyWalletFeature(param)
+    /**
+     * 应用全部 Hook 设置
+     * @param param 正在装载的软件包信息
+     * @param prefs 本地配置缓存
+     */
+    private fun applyAllFeature(
+        param: XposedModuleInterface.PackageReadyParam,
+        prefs: SharedPreferences
+    ) {
+        // 全局搜索 (com.heytap.quicksearchbox)
+        SearchHook.applySearchFeature(
+            module = this,
+            param = param,
+            prefs = prefs
+        )
+        // 系统桌面 (com.android.launcher)
+        HomeHook.applyHomeFeature(
+            module = this,
+            param = param,
+            prefs = prefs
+        )
+        // 系统界面 (com.android.systemui)
+        SystemuiHook.applySystemuiFeature(
+            module = this,
+            param = param,
+            prefs = prefs
+        )
+        // 息屏 (com.oplus.aod)
+        AodHook.applyAodFeature(
+            module = this,
+            param = param,
+            prefs = prefs
+        )
+        // 钱包 (com.finshell.wallet)
+        WalletHook.applyWalletFeature(
+            module = this,
+            param = param,
+            prefs = prefs
+        )
     }
 
-}*/
+    /**
+     * 我 Hook 我自己
+     */
+    private fun hookMine() {
+        if (BuildConfig.APPLICATION_ID == packageName) {
+            MainActivity.Companion::class.java.name.hookMethod(
+                classLoader,
+                "isModuleActive"
+            ) { true }
+            return
+        }
+    }
+}
