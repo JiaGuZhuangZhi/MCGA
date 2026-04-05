89613731cdb6771d07250a128e78d5fb34b2d477
JiaGuZhuangZhi <2280718448@qq.com>
2026-04-05 12:44:58 +0800

    chore(HomeHook): 构造函数迁移至 MainHook

--- app/src/main/java/com/gustate/mcga/xposed/home/HomeHook.kt
+++ app/src/main/java/com/gustate/mcga/xposed/home/HomeHook.kt
@@ -13,7 +13,7 @@ import com.gustate.mcga.xposed.home.feature.RecentsHook
 import io.github.libxposed.api.XposedModule
 import io.github.libxposed.api.XposedModuleInterface
 
-class HomeHook : XposedModule() {
+object HomeHook {
 
     // 实例化相关 Feature 类
     private val dockHook = DockHook()
@@ -21,31 +21,34 @@ class HomeHook : XposedModule() {
     private val recentsHook = RecentsHook()
 
     /**
-     * 成功实例化软件包加载器
+     * 应用系统桌面 Hook 设置
+     * @param module 当前 XposedModule 实例
      * @param param 正在装载的软件包信息
+     * @param prefs 本地配置缓存
      */
-    override fun onPackageReady(param: XposedModuleInterface.PackageReadyParam) {
-        super.onPackageReady(param)
-
+    fun applyHomeFeature(
+        module: XposedModule,
+        param: XposedModuleInterface.PackageReadyParam,
+        prefs: SharedPreferences
+    ) {
         // 以下代码仅 Hook 系统桌面 (com.android.launcher)
         if (param.packageName != "com.android.launcher") return
 
-        // 获取 Lsposed 远程配置
-        val prefs = getRemotePreferences("mcga_prefs")
-
         // 应用配置
-        applyDockFeature(param = param, prefs = prefs)
-        applyDrawerFeature(param = param, prefs = prefs)
-        applyRecentsFeature(param = param, prefs = prefs)
+        applyDockFeature(module = module, param = param, prefs = prefs)
+        applyDrawerFeature(module = module, param = param, prefs = prefs)
+        applyRecentsFeature(module = module, param = param, prefs = prefs)
 
     }
 
     /**
      * 应用 Dock 栏配置
+     * @param module 当前 XposedModule 实例
      * @param param 正在装载的软件包信息
      * @param prefs 本地配置缓存
      */
     private fun applyDockFeature(
+        module: XposedModule,
         param: XposedModuleInterface.PackageReadyParam,
         prefs: SharedPreferences
     ) {
@@ -55,7 +58,7 @@ class HomeHook : XposedModule() {
             val blurRadius = prefs.getInt(DOCK_BLUR_RADIUS, 800)
             val cornerRadius = prefs.getFloat(DOCK_CORNER_RADIUS, 28f)
             dockHook.hookDock(
-                module = this,
+                module = module,
                 param = param,
                 enableDockBlur = enableDockBlur,
                 blurRadius = blurRadius,
@@ -66,17 +69,19 @@ class HomeHook : XposedModule() {
 
     /**
      * 应用抽屉配置
+     * @param module 当前 XposedModule 实例
      * @param param 正在装载的软件包信息
      * @param prefs 本地配置缓存
      */
     private fun applyDrawerFeature(
+        module: XposedModule,
         param: XposedModuleInterface.PackageReadyParam,
         prefs: SharedPreferences
     ) {
         val goneDrawerAppName = prefs.getBoolean(HIDE_DRAWER_NAME, false)
         if (goneDrawerAppName) {
             drawerHook.hideDrawerAppName(
-                module = this,
+                module = module,
                 param = param
             )
         }
@@ -84,17 +89,19 @@ class HomeHook : XposedModule() {
 
     /**
      * 应用最近任务配置
+     * @param module 当前 XposedModule 实例
      * @param param 正在装载的软件包信息
      * @param prefs 本地配置缓存
      */
     private fun applyRecentsFeature(
+        module: XposedModule,
         param: XposedModuleInterface.PackageReadyParam,
         prefs: SharedPreferences
     ) {
         val customizeClearAllBtn = prefs.getBoolean(CLEAR_ALL_BUTTON, false)
         if (customizeClearAllBtn) {
             recentsHook.customizeClearAllButton(
-                module = this,
+                module = module,
                 param = param
             )
         }
