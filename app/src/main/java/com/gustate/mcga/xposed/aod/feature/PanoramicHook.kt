c7d426e71e52954936ac5f13219310322f332c3c
JiaGuZhuangZhi <2280718448@qq.com>
2026-04-10 00:03:08 +0800

    chore(AOD): 细化部分日志

--- app/src/main/java/com/gustate/mcga/xposed/aod/feature/PanoramicHook.kt
+++ app/src/main/java/com/gustate/mcga/xposed/aod/feature/PanoramicHook.kt
@@ -24,20 +24,21 @@ class PanoramicHook {
         module: XposedModule,
         param: XposedModuleInterface.PackageReadyParam
     ) {
-        val settingsUtilsClass = loadClass(
-            "com.oplus.aod.util.SettingsUtils",
-            param.classLoader
-        ) ?: return
-
         try {
-            // 获取目标方法：getKeyAodAllDaySupportSettings(Context, int)
+            // 获取目标类与方法
+            val settingsUtilsClass = loadClass(
+                className = "com.oplus.aod.util.SettingsUtils",
+                classLoader = param.classLoader
+            ) ?: return log(
+                module = module, tag = AOD_LOG,
+                message = "❌ 未获取到 OplusQSResizeableTileView 类"
+            )
             val method = settingsUtilsClass.getDeclaredMethod(
                 "getKeyAodAllDaySupportSettings",
                 Context::class.java,
                 Int::class.javaPrimitiveType
             )
-
-            // 拦截并强制返回 1 (支持全天候)
+            // 拦截并强制返回 1
             module.hook(method).intercept {
                 log(
                     module = module, tag = AOD_LOG,
@@ -54,41 +55,3 @@ class PanoramicHook {
         }
     }
 }
-
-/*package com.gustate.mcga.xposed.aod.feature
-
-import android.content.Context
-import com.gustate.mcga.utils.LogUtils.log
-import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XposedHelpers
-import de.robv.android.xposed.callbacks.XC_LoadPackage
-
-object PanoramicHook {
-    fun enableAllDayAodSettings(lpparam: XC_LoadPackage.LoadPackageParam) {
-        try {
-            val oplusHotseatClass = XposedHelpers.findClass(
-                "com.oplus.aod.util.SettingsUtils",
-                lpparam.classLoader
-            )
-            XposedHelpers.findAndHookMethod(
-                oplusHotseatClass,
-                "getKeyAodAllDaySupportSettings",
-                Context::class.java,
-                Int::class.java,
-                object : XC_MethodHook() {
-                    override fun beforeHookedMethod(param: MethodHookParam) {
-                        log(tag = "息屏", message = "✅ 成功启用全天候息屏设置")
-                        param.result = 1
-                    }
-                }
-            )
-        } catch (e: Exception) {
-            log(
-                message = "❌ 启用全天候息屏设置失败" +
-                        "错误信息: ${e.message}," +
-                        "详情可在 com.gustate.mcga 中查看",
-                tag = "息屏"
-            )
-        }
-    }
-}*/
