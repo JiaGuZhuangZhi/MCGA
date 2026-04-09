bc39c18553ba161c4a174e573a8e0c5ada23d281
JiaGuZhuangZhi <2280718448@qq.com>
2026-04-09 22:03:43 +0800

    chore(Drawer): 细化部分日志

--- app/src/main/java/com/gustate/mcga/xposed/home/feature/DrawerHook.kt
+++ app/src/main/java/com/gustate/mcga/xposed/home/feature/DrawerHook.kt
@@ -19,33 +19,42 @@ class DrawerHook {
         module: XposedModule,
         param: XposedModuleInterface.PackageReadyParam
     ) {
-
         // 获取 ClassLoader 实例
         val classLoader = param.classLoader
-
         // 加载类
         // 加载 Adapter 类
         val baseAdapterClass = loadClass(
             className = "com.android.launcher3.allapps.BaseAllAppsAdapter",
             classLoader = classLoader
+        ) ?: return log(
+            module = module, tag = DRAWER_TAG,
+            message = "❌ 未获取到 BaseAllAppsAdapter 类"
         )
         // 加载 BubbleTextView
         val bubbleTextViewClass = loadClass(
             className = "com.android.launcher3.BubbleTextView",
             classLoader = classLoader
+        ) ?: return log(
+            module = module, tag = DRAWER_TAG,
+            message = "❌ 未获取到 BubbleTextView 类"
         )
         // 找到内部类 ViewHolder
         val viewHolder = loadClass(
             className = "com.android.launcher3.allapps.BaseAllAppsAdapter\$ViewHolder",
             classLoader = classLoader
+        ) ?: return log(
+            module = module, tag = DRAWER_TAG,
+            message = "❌ 未获取到 BaseAllAppsAdapter\$ViewHolder 类"
         )
-
         // 匹配 onBindViewHolder 方法
-        val onBindViewMethod = baseAdapterClass?.getDeclaredMethod(
+        val onBindViewMethod = baseAdapterClass.getDeclaredMethod(
             "onBindViewHolder",
             viewHolder,
             Int::class.javaPrimitiveType
-        ) ?: throw NullPointerException()
+        ) ?: return log(
+            module = module, tag = DRAWER_TAG,
+            message = "❌ 未获取到 onBindViewHolder 函数"
+        )
         // 执行 Hook
         module.hook(onBindViewMethod).intercept { chain ->
             val result = chain.proceed()
@@ -54,69 +63,24 @@ class DrawerHook {
             // 反射获取 itemView (ViewHolder 的公有字段)
             val itemView = viewHolder.javaClass.getField("itemView").get(viewHolder)
             // 如果是 BubbleTextView，直接执行操作
-            if (bubbleTextViewClass?.isInstance(itemView) == true) {
+            if (bubbleTextViewClass.isInstance(itemView)) {
                 itemView.apply {
                     callAnyMethod<Unit>("setText", "")
                     callAnyMethod<Unit>("setMaxLines", 0)
                     callAnyMethod<Unit>("setCompoundDrawablePadding", 0)
                 }
+                log(
+                    module = module, tag = DRAWER_TAG,
+                    message = "✅ 抽屉应用名隐藏成功"
+                )
+                result
+            } else {
+                log(
+                    module = module, tag = DRAWER_TAG,
+                    message = "❌ 隐藏抽屉应用名称失败"
+                )
                 result
             }
-            log(
-                module = module,
-                tag = DRAWER_TAG,
-                message = "✅ 抽屉应用名隐藏成功"
-            )
         }
-
     }
-
 }
-
-/*object DrawerAppName {
-    fun goneDrawerAppName(lpparam: XC_LoadPackage.LoadPackageParam) {
-        try {
-            val baseAdapterClass = XposedHelpers.findClass(
-                "com.android.launcher3.allapps.BaseAllAppsAdapter",
-                lpparam.classLoader
-            )
-
-            val bubbleTextViewClass = XposedHelpers.findClass(
-                "com.android.launcher3.BubbleTextView",
-                lpparam.classLoader
-            )
-
-            //Hook onBindViewHolder(ViewHolder, int)
-            val onBindViewMethod = baseAdapterClass.getDeclaredMethod(
-                "onBindViewHolder",
-                baseAdapterClass.declaredClasses.first {
-                    it.simpleName == "ViewHolder"
-                },
-                Int::class.javaPrimitiveType
-            )
-
-            XposedBridge.hookMethod(
-                onBindViewMethod,
-                object : XC_MethodHook() {
-                    override fun afterHookedMethod(param: MethodHookParam) {
-                        val viewHolder = param.args[0]
-                        // val viewType = XposedHelpers.callMethod(viewHolder, "getItemViewType") as Int
-                        // if (viewType != 2) return // 只处理应用图标
-                        val itemView = XposedHelpers.getObjectField(viewHolder, "itemView")
-                        if (bubbleTextViewClass.isInstance(itemView)) {
-                            // 清除文本
-                            XposedHelpers.callMethod(itemView, "setText", "")
-                            // 移除文字高度
-                            XposedHelpers.callMethod(itemView, "setMaxLines", 0)
-                            // 可选：移除 drawable 和 text 之间的 padding
-                            XposedHelpers.callMethod(itemView, "setCompoundDrawablePadding", 0)
-                        }
-                    }
-                }
-            )
-        } catch (e: Throwable) {
-            XposedBridge.log("HideAppNameHook failed: ${e.message}")
-            e.printStackTrace()
-        }
-    }
-}*/
