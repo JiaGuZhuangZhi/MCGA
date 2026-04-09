fbd49f59bf3f99c17b16827b996e65cd3d6fad13
JiaGuZhuangZhi <2280718448@qq.com>
2026-04-10 00:00:20 +0800

    feat(Home): 新增 Dock 栏详细设置

--- app/src/main/java/com/gustate/mcga/data/state/HomeUiState.kt
+++ app/src/main/java/com/gustate/mcga/data/state/HomeUiState.kt
@@ -1,7 +1,10 @@
 package com.gustate.mcga.data.state
 
 data class HomeUiState(
+    val enableDockBkg: Boolean = false,
     val enableDockBlur: Boolean = false,
+    val dockBlurRadius: Int = 800,
+    val dockCornerRadius: Float = 28f,
     val hideDrawerName: Boolean = false,
     val clearAllButton: Boolean = false
 )
