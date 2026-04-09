e16f07c8f89bdc87f837598dee6a2ae2c58771fe
JiaGuZhuangZhi <2280718448@qq.com>
2026-04-10 00:05:19 +0800

    chore(Recents): 细化部分日志

--- app/src/main/java/com/gustate/mcga/xposed/home/feature/RecentsHook.kt
+++ app/src/main/java/com/gustate/mcga/xposed/home/feature/RecentsHook.kt
@@ -22,110 +22,78 @@ class RecentsHook {
         module: XposedModule,
         param: XposedModuleInterface.PackageReadyParam
     ) {
-
         // 获取 ClassLoader 实例
         val classLoader = param.classLoader
-
-        // 加载类
-        // 加载清除按钮类
-        val panelClass = loadClass(
-            "com.oplus.quickstep.views.OplusClearAllPanelView",
-            classLoader
-        )
-
-        // hook 布局加载完成
-        val onFinishInflate = panelClass
-            ?.getDeclaredMethod("onFinishInflate")
-            ?: throw NullPointerException()
-        module.hook(onFinishInflate).intercept { chain ->
-            // 执行原逻辑
-            val result = chain.proceed()
-            // 取 View
-            val panelView = chain.thisObject as? ViewGroup
-                ?: throw NullPointerException()
-            val clearBtn = panelView.getAnyField<TextView>("mClearAllBtn")
-            // 替换按钮文字
-            clearBtn?.apply {
-                text = "✕"
-                textSize = 20f
-            }
-            log(
+        try {
+            // 加载类
+            // 加载清除按钮类
+            val panelClass = loadClass(
+                className = "com.oplus.quickstep.views.OplusClearAllPanelView",
+                classLoader = classLoader
+            ) ?: return log(
                 module = module, tag = TAG,
-                message = "✅ 已成功修改最近任务按钮"
+                message = "❌ 未获取到 OplusClearAllPanelView 类"
             )
-            result
-        }
-
-        // hook 尺寸更新逻辑 (实现正圆)
-        // 这个方法在 ColorOS 里负责计算按钮的宽高
-        val updateSizeMethod = panelClass.getDeclaredMethod("updateClearAllSize")
-        module.hook(updateSizeMethod).intercept { chain ->
-            // 执行原逻辑
-            val result = chain.proceed()
-            // 取 View
-            val panelView = chain.thisObject as? ViewGroup
-                ?: throw NullPointerException()
-            val clearBtn = panelView.getAnyField<View>("mClearAllBtn")
-
-            // 将宽度强制设为与高度一致，形成 1:1 正方形
-            clearBtn?.layoutParams?.let { lp ->
-                if (clearBtn.measuredHeight > 0) {
-                    lp.width = clearBtn.measuredHeight
-                    clearBtn.layoutParams = lp
-                    log(
-                        module = module, tag = TAG,
-                        message = "✅ 已重置清除按钮比例为 1:1"
-                    )
+            // hook 布局加载完成
+            val onFinishInflate = panelClass
+                .getDeclaredMethod("onFinishInflate")
+                ?: return log(
+                    module = module, tag = TAG,
+                    message = "❌ 未获取到 onFinishInflate 函数"
+                )
+            module.hook(onFinishInflate).intercept { chain ->
+                // 执行原逻辑
+                val result = chain.proceed()
+                // 取 View
+                val panelView = chain.thisObject as? ViewGroup
+                    ?: throw NullPointerException()
+                val clearBtn = panelView.getAnyField<TextView>("mClearAllBtn")
+                // 替换按钮文字
+                clearBtn?.apply {
+                    text = "✕"
+                    textSize = 20f
                 }
+                log(
+                    module = module, tag = TAG,
+                    message = "✅ 已成功修改最近任务按钮"
+                )
+                result
             }
-            result
-        }
-    }
-}
 
-/*object Recent {
-    fun changeClearAllButton(
-        lpparam: XC_LoadPackage.LoadPackageParam
-    ) {
-        val panelClassName = "com.oplus.quickstep.views.OplusClearAllPanelView"
-        XposedHelpers.findAndHookMethod(
-            panelClassName,
-            lpparam.classLoader,
-            "onFinishInflate",
-            object : XC_MethodHook() {
-                override fun afterHookedMethod(param: MethodHookParam) {
-                    val panelView = param.thisObject as ViewGroup
-                    // 通过反射或字段名获取 mClearAllBtn
-                    // 代码显示该字段为 public，可以直接获取
-                    val clearBtn = XposedHelpers
-                        .getObjectField(panelView, "mClearAllBtn") as? TextView
-                        ?: return
+            // hook 尺寸更新逻辑 (实现正圆)
+            // 这个方法在 ColorOS 里负责计算按钮的宽高
+            val updateSizeMethod = panelClass
+                .getDeclaredMethod("updateClearAllSize")
+                ?: return log(
+                    module = module, tag = TAG,
+                    message = "❌ 未获取到 updateClearAllSize 函数"
+                )
+            module.hook(updateSizeMethod).intercept { chain ->
+                // 执行原逻辑
+                val result = chain.proceed()
+                // 取 View
+                val panelView = chain.thisObject as? ViewGroup
+                    ?: throw NullPointerException()
+                val clearBtn = panelView.getAnyField<View>("mClearAllBtn")
 
-                    // 修改文字为 ✕
-                    clearBtn.text = "✕"
-                    clearBtn.textSize = 20f
-                }
-            }
-        )
-        XposedHelpers.findAndHookMethod(
-            panelClassName,
-            lpparam.classLoader,
-            "updateClearAllSize",
-            object : XC_MethodHook() {
-                override fun afterHookedMethod(param: MethodHookParam) {
-                    val panelView = param.thisObject as ViewGroup
-                    val clearBtn = XposedHelpers.getObjectField(
-                        panelView,
-                        "mClearAllBtn"
-                    ) as? android.view.View ?: return
-
-                    val lp = clearBtn.layoutParams
+                // 将宽度强制设为与高度一致，形成 1:1 正方形
+                clearBtn?.layoutParams?.let { lp ->
                     if (clearBtn.measuredHeight > 0) {
                         lp.width = clearBtn.measuredHeight
                         clearBtn.layoutParams = lp
                     }
                 }
+                result
             }
-        )
+            log(
+                module = module, tag = TAG,
+                message = "✅ 已重置清除按钮比例为 1:1"
+            )
+        } catch (e: Exception) {
+            log(
+                module = module, tag = TAG,
+                message = "❌ 最近任务按钮修改失败 错误信息: ${e.message}"
+            )
+        }
     }
-}*/
+}
