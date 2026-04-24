d509bdda847cfd1c257dd4169b60283ce93db4fa
JiaGuZhuangZhi <2280718448@qq.com>
2026-04-25 03:29:37 +0800

    chore(*): 区分详细日志与错误日志

--- app/src/main/java/com/gustate/mcga/utils/LogUtils.kt
+++ app/src/main/java/com/gustate/mcga/utils/LogUtils.kt
@@ -7,7 +7,7 @@ import io.github.libxposed.api.XposedModule
 object LogUtils {
 
     /**
-     * 输出 Log
+     * 输出详细日志 (可关闭)
      * @param module 模块入口类
      * @param priority [Log] 等级
      * @param tag log 标签
@@ -24,6 +24,24 @@ object LogUtils {
             .getBoolean(ModuleKeys.ENABLE_LOG, true)
         if (!logEnabled) return
         module.log(priority, "MCGA-$tag", message)
+    }
+
+    /**
+     * 输出重要日志
+     * @param module 模块入口类
+     * @param priority [Log] 等级
+     * @param tag log 标签
+     * @param message log 信息
+     * @param throwable 所需输出的异常
+     */
+    fun log(
+        module: XposedModule,
+        priority: Int = Log.DEBUG,
+        tag: String = "未设置",
+        message: String,
+        throwable: Throwable
+    ) {
+        module.log(priority, "MCGA-$tag", message, throwable)
     }
 
 }
